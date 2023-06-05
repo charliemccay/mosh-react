0d17995c0b1c32250552d6b37df02c6adc4f5a9a
charliemccay <charlie@ramseysystems.co.uk>
2023-06-05 17:37:05 +0100

    added list items as constants

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,13 @@
 function ListGroup() {
+    const items = ["new york", "London", "Paris", "Berlin"];
+
     return (
         <>
             <h1>The heading</h1>
             <ul className="list-group">
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
+                {items.map((item) => (
+                    <li>{item}</li>
+                ))}
             </ul>
         </>);
 }
